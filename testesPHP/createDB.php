65c2605b1df60c3aaa37bc9b286bdf390743c7ce
Luis Louro <lapisdecor@gmail.com>
2015-07-14 11:17:00 +0100

    testa metodo post

--- testesPHP/createDB.php
+++ testesPHP/createDB.php
@@ -1,6 +1,7 @@
 <?php
 
 // liga-se ao mysql
+$password = $_POST['password'];
 $mysqli = mysqli_connect("127.0.0.1", "root", $password);
 if (mysqli_connect_errno($mysqli)) {
     echo "Falha ao ligar-se ao MySql: " . mysqli_connect_error();
@@ -15,11 +16,11 @@ if (!$mysqli->query("CREATE DATABASE saramugo")) {
 }
 
 // usa a base de dados e cria a tabela clientes
-if (!$mysqli->query("USE saramugo") || (!$mysqli->query("CREATE TABLE clientes"))) {
+if (!$mysqli->query("USE saramugo") || (!$mysqli->query("CREATE TABLE clientes {Nome VARCHAR(200), NIF INT, Email VARCHAR(50)}"))) {
   echo "Falha ao criar a tabela clientes: (" . $mysqli->errno . ") " . $mysqli->error;
 }
 
-// insere esquema rudimentar de campos na tabela clientes
+// insere dados de exemplo na tabela clientes
 if (!$mysqli->query("INSERT INTO clientes (Nome, NIF, Email) VALUES ('ClienteDeTeste', 000000000, 'cliente@exemplo.com')")) {
   echo "Falha ao inserir valores de teste: (" . $mysqli->errno . ") " . $mysqli->error;
 } else {
